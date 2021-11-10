bea37b5b1cde326ab4c6afc24a21cf37652c8b2e
dorissuzukiesmerio <dsuzuki@g.clemson.edu>
2021-11-10 16:05:40 -0500

    init

---
+++ uploading-to-github-command-line.py
@@ -0,0 +1,41 @@
+# Uploading to Github using the command line
+# Workflow:
+
+# 1. Open the command (type command on windows or termianal on Mac)
+
+# 2. Go to the path you want your folder to be
+
+# cd project_path
+# Ex: cd C:\Users\doris\Desktop\Learning_Git_and_Github
+
+# Hint : open the folder interface, go to the folder, copy the path and paste it on the terminal
+
+# 3. Create a folder
+
+# mkdir project_name
+
+# 4. Go to the folder (assuming you are already in correct path)
+
+# cd project_name
+
+# 5. Set up git repository
+# git init
+
+# 6. Go to Github and make a new repository
+
+# Arrow: + , new repository, choose unique name (good practice to put same name as the folder you created in your computer)
+# 7. Link your local repo to the github repo
+# (copy lines that github will give you)
+# git remote add origin git@github.com:your_id/project_name.git
+# 8. Go to the folder
+# cd project_path/project_name
+# 9. Write code
+# 10. Add to repository
+# git add .
+# 11. Commit to git repository
+# git commit -m"description"
+# 12. Upload to github
+# git push origin master
+# 13. Go back to 9 until your project is done !
+
+
